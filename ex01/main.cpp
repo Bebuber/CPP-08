8f395ae4d0a8c6950afbd5e8543158e6b89958f7
Betul Buber <bebuber@student.42heilbronn.de>
2025-03-05 17:04:13 +0100

    tests included

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,27 +6,44 @@
 /*   By: bebuber <bebuber@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 19:50:34 by bebuber           #+#    #+#             */
-/*   Updated: 2025/03/05 16:20:04 by bebuber          ###   ########.fr       */
+/*   Updated: 2025/03/05 17:02:22 by bebuber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
-	// Span sp = Span(5);
-	// sp.addNumber(6);
-	// sp.addNumber(3);
-	// sp.addNumber(17);
-	// sp.addNumber(9);
-	// sp.addNumber(11);
+	Span sp = Span(5);
 	
-
-	Span wide = Span(10000);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
 	
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
+	
+	
+	Span span = Span(10000);
+	
+	std::srand(std::time(0));
+	
+	std::vector<int> numbers;
+	for (int i = 0; i < 10000; i++) {
+		numbers.push_back(std::rand());
+	}
 
+	try {
+		span.addNumbers(numbers.begin(), numbers.end());
+		std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
+		std::cout << "Longest span: " << span.longestSpan() << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 	return 0;
 }
