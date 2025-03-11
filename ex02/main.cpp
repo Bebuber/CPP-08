3b0213fe5d563a7b1d78da864afab117dbcfbd5c
Betul Buber <bebuber@student.42heilbronn.de>
2025-03-11 13:56:04 +0100

    added more tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bebuber <bebuber@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 17:06:00 by bebuber           #+#    #+#             */
-/*   Updated: 2025/03/11 13:26:07 by bebuber          ###   ########.fr       */
+/*   Updated: 2025/03/11 13:55:31 by bebuber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,38 @@ int main()
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << "Top element: " << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << "Size after pop: " << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
+	
+	std::cout << "Elements in stack: ";
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << " ";
+		++it;
 	}
-	std::stack<int> s(mstack);
+	std::cout << std::endl;
+
+	std::cout << "Testing copy constructor:" << std::endl;
+    MutantStack<int> copyStack(mstack);
+    for (auto it = copyStack.begin(); it != copyStack.end(); it++) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Testing assignment operator:" << std::endl;
+    MutantStack<int> assignedStack;
+    assignedStack = mstack;
+    for (auto it = assignedStack.begin(); it != assignedStack.end(); it++) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+	
 	return 0;
 }
